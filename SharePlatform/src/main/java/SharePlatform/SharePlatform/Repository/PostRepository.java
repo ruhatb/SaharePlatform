67656da037225b27fe06494cd2b059e97d6acc83
ruhatb <r.uhat@yahoo.com.tr>
2024-06-10 17:59:13 +0300

    first steps entity created

--- SharePlatform/src/main/java/SharePlatform/SharePlatform/Repository/PostRepository.java
+++ SharePlatform/src/main/java/SharePlatform/SharePlatform/Repository/PostRepository.java
@@ -1,5 +1,7 @@
 package SharePlatform.SharePlatform.Repository;
 
+import SharePlatform.SharePlatform.Entity.Posts;
+import SharePlatform.SharePlatform.Entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
